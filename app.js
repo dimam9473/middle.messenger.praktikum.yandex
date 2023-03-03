f6abaf0eca623ff207121c51d71448e662a91973
dmednikov <dimam9473@gmail.com>
2023-03-03 11:41:19 +0300

    added error pages

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-// var express = require('express');
-// var app = express();
-
-// app.get('/', function(req, res) {
-//   res.send('Hello World!');
-// });
-
-// app.listen(3000, function() {
-//   console.log('Example app listening on port 3000!');
-// });
-
 const express = require("express");
 const hbs = require("hbs");
  
@@ -35,7 +24,6 @@ app.use("/profile", function(_, response){
 });
 
 app.use("/", function(_, response){
-     
     response.render("pages/login/login.hbs");
 });
 
