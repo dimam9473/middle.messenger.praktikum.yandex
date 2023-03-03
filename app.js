d4b5e3268eed8ab7bb21de97ca96b5b66b8576a5
dmednikov <dimam9473@gmail.com>
2023-03-03 11:48:20 +0300

    modified routing

--- app.js
+++ app.js
@@ -8,24 +8,32 @@ app.set("views", "src");
 hbs.registerPartials(`${__dirname}/src/components`);
 app.set("view engine", "hbs");
 
-app.use("/register", function(_, response){
+app.use(express.static('index.html'));
+
+app.get("/", function(_, response){
+    response.render("pages/login/login.hbs");
+});
+
+app.get("/register", function(_, response){
      
     response.render("pages/register/register.hbs");
 });
 
-app.use("/chat", function(_, response){
+app.get("/chat", function(_, response){
      
     response.render("pages/chat/chat.hbs");
 });
 
-app.use("/profile", function(_, response){
+app.get("/profile", function(_, response){
      
     response.render("pages/profile/profile.hbs");
 });
 
-app.use("/", function(_, response){
-    response.render("pages/login/login.hbs");
-});
+app.get("*", (_, response) => {
+    response.status(404).render("pages/404/404.hbs");
+    response.status(500).render("pages/500/500.hbs");
+  });
+    
 
 app.listen(port, function () {
     console.log(`App listening on port: ${port}`)
