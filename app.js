c161a5dae7fdc0e07cdb425bf4ace149384d42d2
dmednikov <dimam9473@gmail.com>
2023-03-05 09:27:13 +0300

    added get in express. uploaded template

--- app.js
+++ app.js
@@ -2,4 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static('/dist'));
+const port = 3000
+
+app.use(express.static(`${__dirname}/dist`));
+app.set('views', `${__dirname}/dist`);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/', (_, response) => {
+    console.log('get')
+    response.render(`${__dirname}/dist/index.html`)
+});
+
+app.listen(port, () => console.log(`App is listening on port: ${port}`));
