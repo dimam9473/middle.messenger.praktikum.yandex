8221f06217f1166fca2167d66bb0b8dd94a79bf3
dmednikov <dimam9473@gmail.com>
2023-03-05 10:02:41 +0300

    removed set views

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const app = express();
 const port = 3000
 
 app.use(express.static(`${__dirname}/dist`));
-app.set('views', `${__dirname}/dist`);
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.get("/*", (_, res) => {
-    res.sendFile(__dirname + '/dist/index.html');
+    res.sendFile(`${__dirname}/dist/index.html`);
 });
 
 app.listen(port, () => console.log(`App is listening on port: ${port}`));
