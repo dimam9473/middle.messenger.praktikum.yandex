90175bd9759a5580f42fa5af55fb881efbc0e588
dmednikov <dimam9473@gmail.com>
2023-03-05 14:39:28 +0300

    removed redirect from netlify. added dev command. add navigation to main page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import mountLogin from "./pages/login/login"
 import mountRegister from "./pages/register/register"
 import mountChat from "./pages/chat/chat"
 import mountProfile from "./pages/profile/profile"
+import mountServerError from "./pages/500/500"
 import mountNotFound from "./pages/404/404"
 
 window.addEventListener("load", function () {
@@ -22,6 +23,11 @@ window.addEventListener("load", function () {
 
     if (window.location.pathname === '/profile') {
         mountProfile('root')
+        return
+    }
+
+    if (window.location.pathname === '/500') {
+        mountServerError('root')
         return
     }
 
