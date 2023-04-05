56aa859a0e61167a4b05afc0f390840616cf6547
dmednikov <dimam9473@gmail.com>
2023-04-05 10:40:08 +0300

    removed comments

--- src/index.ts
+++ src/index.ts
@@ -13,29 +13,3 @@ window.addEventListener('load', function () {
         .use('/500', ServerError)
         .start();
 })
-
-// Необходимо оставить в силу особенностей тренажёра
-// history.pushState({}, '', '/');
-
-// const router = new Router('.app');
-
-// // Можно обновиться на /user и получить сразу пользователя
-// router
-//     .use('/', Login)
-//     .use('/users', Profile)
-//     .start();
-
-// // Через секунду контент изменится сам, достаточно дёрнуть переход
-// setTimeout(() => {
-//     router.go('/users');
-// }, 1000);
-
-// // А можно и назад
-// setTimeout(() => {
-//     router.back();
-// }, 3000);
-
-// // И снова вперёд
-// setTimeout(() => {
-//     router.forward();
-// }, 5000);
